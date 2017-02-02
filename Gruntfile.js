4f96334c427cbf1783f3590f6b91ee932eb6123b
Rémi Bèges <remi.beges@gmail.com>
2017-02-02 14:19:46 +0100

    Updated Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,5 @@
 /*
- * phd-summary
- * 
- * Licensed under none License
+ * Remi Beges PhD final report
  */
 module.exports = function (grunt) {
   'use strict';
@@ -12,56 +10,79 @@ module.exports = function (grunt) {
         src: 'main.tex',
         options: {
           outputDirectory: 'dist/',
-          jobname: 'phd-summary'
+          jobname: 'phd',
+          shellEscape: true
         }
       },
       bib: {
-        src: 'dist/phd-summary.aux',
+        src: 'dist/phd.aux',
         options: {
           engine: 'bibtex',
           interaction: false
         }
-      },
-      gloss: {
-        src: 'dist/phd-summary.aux',
-        options: {
-          engine: 'makeglossaries',
-          interaction: false
-        }
       }
     },
+    shell: {
+      glossary: {
+        command: 'makeglossaries -d ./dist phd'
+      },
+      figs: {
+        command: function() {
+          return 'inkscape --file="' + grunt.config('shell.figs.src') + '" --export-pdf="' + grunt.config('shell.figs.src').replace('.svg', '.pdf') + '"';
+        },
+        src: '**/*.svg'
+      }
+    },
+    execute: {
+  		figs: {
+  			src: ['figs.js']
+  		}
+  	},
     connect: {
       server: {
         options: {
           hostname: 'localhost',
           base: 'dist',
-          open: 'http://localhost:8000/phd-summary.pdf'
+          open: 'http://localhost:8000/phd.pdf'
         }
       }
     },
     watch: {
       latex: {
         files: '**/*.tex',
-        tasks: ['latex', 'latex:pdf']
+        tasks: ['latex', 'shell:glossary', 'latex:pdf']
       },
       bibtex: {
         files: '**/*.bib',
         tasks: ['latex:bib', 'latex:pdf']
       },
+      svgs: {
+        files: '**/**.svg',
+        tasks: ['shell:figs'],
+        options: {
+          spawn: false
+        }
+      },
       livereload: {
         options: {
           livereload: true
         },
-        files: ['dist/phd-summary.pdf'],
+        files: ['dist/phd.pdf'],
       },
     }
+  });
+
+  grunt.event.on('watch', function(action, filepath) {
+    grunt.config('shell.figs.src', filepath);
   });
 
   // These plugins provide necessary tasks
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-latex');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-execute');
 
   // Default task
-  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('default', ['execute:figs', 'watch']);
 };
