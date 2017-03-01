be4700e83563ff2ebcb455479bf032220037bc72
Rémi Bèges <remi.beges@gmail.com>
2017-03-01 16:23:58 +0100

    Simplify spellchecking script

--- spellcheck.cmd
+++ spellcheck.cmd
@@ -1,7 +1,2 @@
 hunspell -t -i UTF-8 -d fr-toutesvariantes main.tex
-hunspell -t -i UTF-8 -d fr-toutesvariantes ./src/1/intro.tex
-hunspell -t -i UTF-8 -d fr-toutesvariantes ./src/1/chap1.tex
-hunspell -t -i UTF-8 -d fr-toutesvariantes ./src/1/chap2.tex
-hunspell -t -i UTF-8 -d fr-toutesvariantes ./src/1/chap3.tex
-hunspell -t -i UTF-8 -d fr-toutesvariantes ./src/1/chap4.tex
-hunspell -t -i UTF-8 -d fr-toutesvariantes ./src/1/conclusion.tex
+hunspell -t -i UTF-8 -d fr-toutesvariantes ./src/1/*.tex
